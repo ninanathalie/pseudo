7409d6a0bf75e208d720da17302db1b264b2010d
ninanathalie <mninanathalie@gmail.com>
2024-06-19 23:53:44 +0400

    feat(ui): update intro section

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,16 +1,26 @@
-"use client";
-
 import React from "react";
-import { TextGenerateEffect } from "@/components/ui/text-generate";
 import { Syne } from "next/font/google";
+import Image from "next/image";
 
 const syne = Syne({ subsets: ["latin"] });
-const words = `Hello! My name is Nathalie. When life becomes overwhelming, this is where you'll find me. This is a caboodle of my daily ramblings – an abode for my undistributed thoughts, serene thinking and tranquil ideas. Today, I welcome you to my blog, and these are my misadventures.`;
 
 export default function Intro() {
   return (
-    <section>
-      <TextGenerateEffect words={words} className={`${syne.className}`} />
+    <section className="w-full h-full flex items-center justify-center">
+      <div className="w-full px-10">
+        <div className="flex items-center justify-center gap-4 mb-2">
+          <Image src="/images/home-img-1.jpg" alt="Intro Image" width="142" height="142" quality="95" priority={true} className="w-24 h-24 xl:w-30 xl:h-30 rounded-full object-cover" />
+          <h1 className={`${syne.className} text-right text-8xl font-bold uppercase`}>Hello there</h1>
+        </div>
+        <div className="flex items-center justify-center w-full gap-4 mb-2">
+          <h1 className={`${syne.className}  text-left text-8xl font-bold uppercase`}>My name is</h1>
+          <span className="text-xs font-light w-80 uppercase">This is a caboodle of my daily ramblings – an abode for my undistributed thoughts, serene thinking and tranquil ideas. Today, I welcome you to my blog, and these are my misadventures.</span>
+        </div>
+        <div className="flex items-center justify-center ml-auto gap-4">
+          <h1 className={`${syne.className} text-right text-8xl font-bold uppercase`}>Nathalie</h1>
+          <Image src="/images/home-img-2.jpg" alt="Intro Image" width="242" height="142" quality="95" priority={true} className="w-32 h-24 xl:w-44 xl:h-30 rounded-full object-cover" />
+        </div>
+      </div>
     </section>
   );
 }
