1988c818604dcce3e90144ed511d144a9f7db94c
ninanathalie <mninanathalie@gmail.com>
2024-07-28 03:30:02 +0400

    feat(intro): update intro section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Intro from "@/components/intro";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center flex-1 w-full z-10">
+    <main className="flex flex-col items-center mt-[30%] lg:mt-[20%] xl:mt-[15%] 3xl:mt-[8%] flex-1 w-full z-10">
       <Intro />
     </main>
   );
