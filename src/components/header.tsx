d7e60f4a9fa8b2e0fb3929b6c1304f3f2178f1ff
ninanathalie <mninanathalie@gmail.com>
2024-07-27 23:10:54 +0400

    feat(nav): update floating navbar and header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,44 @@
 import React from "react";
 import { FloatingNav } from "@/components/ui/floating-navbar";
-import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+import { Home, NotebookPen, Sun, FilePlus2, LogIn, LogOut } from "lucide-react";
 
 export default function Header() {
   const navItems = [
     {
       name: "Home",
       link: "/",
-      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <Home className="h-5 w-5" />,
+      requiresAuth: false,
     },
     {
       name: "Blogs",
       link: "/blogs",
-      icon: <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />,
+      icon: <NotebookPen className="h-5 w-5" />,
+      requiresAuth: false,
+    },
+    {
+      name: "Write",
+      link: "/new-blog",
+      icon: <FilePlus2 className="h-5 w-5" />,
+      requiresAuth: true,
+    },
+    {
+      name: "LogIn",
+      link: "/login",
+      icon: <LogIn className="h-5 w-5" />,
+      requiresAuth: false,
+    },
+    {
+      name: "LogOut",
+      link: "",
+      icon: <LogOut className="h-5 w-5" />,
+      requiresAuth: true,
+    },
+    {
+      name: "Light",
+      link: "/",
+      icon: <Sun className="h-5 w-5" />,
+      requiresAuth: false,
     },
   ];
   return (
