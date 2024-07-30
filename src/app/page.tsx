e53596146417ac2ae88d895718dc1edfb99c4ea9
ninanathalie <mninanathalie@gmail.com>
2024-07-30 23:17:25 +0400

    feat(ui): add a moving text in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Intro from "@/components/homepage/intro";
+import MovingText from "@/components/homepage/moving-text";
 import RecentBlogs from "@/components/homepage/recent-blogs";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="flex flex-col items-center mt-[30%] lg:mt-[20%] xl:mt-[15%] 3xl:mt-[8%] flex-1 w-full z-10">
       <Intro />
       <RecentBlogs />
+      <MovingText />
     </main>
   );
 }
