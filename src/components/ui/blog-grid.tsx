e019bc92721302edba012f72b41e129a76f49d70
nathalie <mninanathalie@gmail.com>
2024-10-30 14:50:09 +0400

    fix(error): resolve hydration error in blog-grid by deferring Link rendering

--- src/components/ui/blog-grid.tsx
+++ src/components/ui/blog-grid.tsx
@@ -1,12 +1,26 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { cn } from "@/utils/cn";
-import Link from "next/link";
 import { RiArrowRightUpLine } from "react-icons/ri";
+import Link from "next/link";
 
 export const BlogGrid = ({ className, children }: { className?: string; children?: React.ReactNode }) => {
   return <div className={cn("custom-blog gap-4 w-full lg:max-w-5xl 4xl:max-w-6xl mx-auto", className)}>{children}</div>;
 };
 
 export const BlogGridItem = ({ className, title, description, date, link }: { className?: string; title?: string | React.ReactNode; description?: string | React.ReactNode; date?: string; link?: string }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  const excerpt = description ? description.toString() : "";
+
+  useEffect(() => {
+    setIsMounted(true); // Ensures the component has mounted
+  }, []);
+
+  if (!isMounted) {
+    return null; // Don't render anything on the server-side
+  }
+
   return (
     <Link href={link ?? "/"} className={cn("group/blog transition duration-200 justify-between flex flex-col py-6 border-b", className)}>
       <div className="group-hover/blog:translate-x-2 transition duration-200">
@@ -20,7 +34,7 @@ export const BlogGridItem = ({ className, title, description, date, link }: { cl
               <p className="font-light text-neutral-400 dark:text-neutral-300 text-xs uppercase ml-2 flex-shrink-0 whitespace-nowrap">• {date}</p>
             </div>
 
-            <p className="truncate font-light text-neutral-600 dark:text-neutral-400 md:text-lg w-full" dangerouslySetInnerHTML={{ __html: description || "" }} />
+            <p className="truncate font-light text-neutral-600 dark:text-neutral-400 md:text-lg w-full" dangerouslySetInnerHTML={{ __html: excerpt }} />
           </div>
         </div>
       </div>
