8ea3fa67ecd3e1f418d47e878edf5eab274ed08f
nathalie <mninanathalie@gmail.com>
2024-10-30 12:15:29 +0400

    style(ui): updated blog list container

--- src/components/homepage/recent-blogs.tsx
+++ src/components/homepage/recent-blogs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid";
+import { BlogGrid, BlogGridItem } from "@/components/ui/blog-grid";
 import BackgroundBlob from "@/components/ui/background-blob";
 import { format } from "date-fns";
 import prisma from "@/lib/db";
@@ -14,12 +14,12 @@ export default async function RecentBlogs() {
   });
 
   return (
-    <section className="relative flex items-center justify-center flex-col w-10/12 3xl:w-2/4 max-w-[1440px]">
+    <section className="relative flex items-center justify-center flex-col w-10/12 3xl:w-2/4 max-w-[1440px] mb-[25%] lg:mb-[20%] 2xl:mb-[10%]">
       <BackgroundBlob variant="center" />
 
       <h2 className="font-polysans-bold text-center text-4xl lg:text-6xl mb-6">Fresh Musings</h2>
-      <div className="flex items-center justify-center mt-6 lg:mt-10 gap-4">
-        <BentoGrid className="mx-auto md:auto-rows-[20rem]">
+      <div className="flex items-center justify-center mt-6 lg:mt-10 gap-4 w-full">
+        <BlogGrid className="mx-auto md:auto-rows-[20rem]">
           {recentPosts.map((post, index) => {
             const formattedDate = format(new Date(post.createdAt), "MMMM dd, yyyy");
             const gridClass = index === 0 || index === 3 ? "md:col-span-2" : "md:col-span-1";
@@ -33,9 +33,9 @@ export default async function RecentBlogs() {
               .replace(/<br\s*\/?>/g, "")
               .trim();
 
-            return <BentoGridItem key={post.id} title={post.title} description={cleanExcerpt} date={formattedDate} link={`/blogs/${post.slug}`} className={gridClass} />;
+            return <BlogGridItem key={post.id} title={post.title} description={cleanExcerpt} date={formattedDate} link={`/blogs/${post.slug}`} className={gridClass} />;
           })}
-        </BentoGrid>
+        </BlogGrid>
       </div>
       <div className="flex items-center justify-center mt-10 lg:mt-16 gap-4">
         <Link href="/blogs" className="font-polysans-regular text-md md:text-lg lg:text-xl px-6 py-3 text-white bg-neutral-950 dark:text-neutral-950 dark:bg-neutral-50 rounded-xl">
