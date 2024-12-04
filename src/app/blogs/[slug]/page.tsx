6ed8dcb912b59becd25fda5f542703cc582899d9
nathalie <mninanathalie@gmail.com>
2024-12-04 17:06:14 +0400

    feat(animation): add blur-fade on login and single blog page

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -8,6 +8,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import BackgroundBlob from "@/components/ui/background-blob";
 import DeleteButton from "@/components/ui/delete-btn";
+import { BlurFade } from "@/components/ui/blur-fade";
 
 export default async function SinglePage({ params }: { params: { slug: string } }) {
   const { isAuthenticated } = getKindeServerSession();
@@ -34,34 +35,41 @@ export default async function SinglePage({ params }: { params: { slug: string } 
   return (
     <main className="relative w-full flex flex-col items-center justify-center mb-16 lg:mb-24 mt-32 lg:mt-44 z-10">
       <BackgroundBlob variant="center" />
-      <div className="mb-8 text-center max-w-4xl">
-        <h1 className="text-4xl md:text-6xl font-polysans-bold mb-2 leading-10 px-6">{post.title}</h1>
-        <small className="mb-5 text-neutral-400 uppercase font-polysans-thin">{formattedDate}</small>
-      </div>
+      <BlurFade delay={0.25} inView className="w-full md:max-w-4xl flex justify-center">
+        <div className="mb-8 text-center max-w-4xl">
+          <h1 className="text-4xl md:text-6xl font-polysans-bold mb-2 leading-10 px-6">{post.title}</h1>
+          <small className="mb-5 text-neutral-400 uppercase font-polysans-thin">{formattedDate}</small>
+        </div>
+      </BlurFade>
       {(await isAuthenticated()) && (
         <div className="hidden md:flex max-w-fit fixed top-6 right-4 border border-transparent rounded-2xl z-40 ">
           <FloatingNav items={navItems} />
         </div>
       )}
-      <div className="natsu-blog flex flex-col items-center justify-center" dangerouslySetInnerHTML={{ __html: post.body || "" }} />
-      <div className="flex justify-between items-start w-full mt-8 md:max-w-4xl px-6 gap-2">
-        {previousPost && (
-          <Link href={`/blogs/${previousPost.slug}`}>
-            <span className="flex flex-col w-44 md:w-60 p-6 bg-white/20 border rounded-xl text-right">
-              <span className="text-xs mb-2 uppercase text-neutral-400">← Previous Post</span>
-              <span className="truncate font-polysans-bold text-md">{previousPost.title}</span>
-            </span>
-          </Link>
-        )}
-        {latestPost && (
-          <Link href={`/blogs/${latestPost.slug}`}>
-            <span className="flex flex-col w-44 md:w-60 p-6 bg-white/20 border rounded-xl">
-              <span className="text-xs mb-2 uppercase text-neutral-400">Latest Post →</span>
-              <span className="truncate font-polysans-bold text-md">{latestPost.title}</span>
-            </span>
-          </Link>
-        )}
-      </div>
+      <BlurFade delay={0.5} inView className="w-full md:max-w-4xl flex justify-center">
+        <div className="natsu-blog flex flex-col items-center justify-center" dangerouslySetInnerHTML={{ __html: post.body || "" }} />
+      </BlurFade>
+
+      <BlurFade delay={0.75} inView className="w-full md:max-w-4xl flex justify-center">
+        <div className="flex justify-between items-start w-full mt-8 md:max-w-4xl px-6 lg:px-0 gap-2">
+          {previousPost && (
+            <Link href={`/blogs/${previousPost.slug}`}>
+              <span className="flex flex-col w-44 md:w-60 p-6 bg-white/20 border rounded-xl text-right">
+                <span className="text-xs mb-2 uppercase text-neutral-400">← Previous Post</span>
+                <span className="truncate font-polysans-bold text-md">{previousPost.title}</span>
+              </span>
+            </Link>
+          )}
+          {latestPost && (
+            <Link href={`/blogs/${latestPost.slug}`}>
+              <span className="flex flex-col w-44 md:w-60 p-6 bg-white/20 border rounded-xl">
+                <span className="text-xs mb-2 uppercase text-neutral-400">Latest Post →</span>
+                <span className="truncate font-polysans-bold text-md">{latestPost.title}</span>
+              </span>
+            </Link>
+          )}
+        </div>
+      </BlurFade>
     </main>
   );
 }
