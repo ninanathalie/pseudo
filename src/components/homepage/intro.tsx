ddcc2fab04e497ca5714d439c136d2d0add90251
nathalie <mninanathalie@gmail.com>
2024-12-04 17:41:56 +0400

    feat(intro): update meta title, favicon, and intro section

--- src/components/homepage/intro.tsx
+++ src/components/homepage/intro.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import BackgroundBlob from "@/components/ui/background-blob";
-import { BlurFade } from "@/components/ui/blur-fade";
 import { HyperText } from "@/components/ui/hyper-text";
+import { BlurFade } from "@/components/ui/blur-fade";
 import { Space_Mono } from "next/font/google";
 
 const spaceMono = Space_Mono({
@@ -12,19 +12,19 @@ const spaceMono = Space_Mono({
 
 export default function Intro() {
   return (
-    <section className="relative max-w-[1440px] w-10/12 3xl:w-2/4">
+    <section className="relative flex items-center justify-center flex-col w-10/12 3xl:w-2/4 max-w-[1440px] mb-[4%] 4xl:mb-[2%]">
       <BackgroundBlob variant="top" />
 
       <BlurFade delay={0.25} inView>
         <div className="flex items-center justify-center flex-col">
-          <Image src="/images/home-img-1.jpg" alt="Intro Image" width="600" height="600" quality="95" priority={true} className="w-24 h-24 xl:w-32 xl:h-32 rounded-full object-cover mb-6 md:mb-8 border-4 border-white dark:border-white/10" />
+          <Image src="/images/home-img-1.png" alt="Intro Image" width="420" height="420" quality="95" priority={true} className="w-24 h-24 xl:w-36 xl:h-36 object-cover md:mb-6" />
 
-          <h1 className="font-polysans-bold text-center text-5xl md:text-6xl lg:text-8xl font-bold mb-2 md:mb-4">Hello, I'm Nathalie</h1>
+          <HyperText className={`${spaceMono.className} text-center text-md md:text-2xl tracking-tighter md:mb-2 lg:mb-6`} text={`while(!(succeed=try())){}`} />
 
-          <HyperText className={`${spaceMono.className} text-center text-lg md:text-2xl lg:text-3xl tracking-tighter mb-2 lg:mb-6`} text={`No pseudo pretenses here. Just me.`} />
+          <h1 className="font-polysans-bold text-center text-4xl md:text-6xl lg:text-8xl font-bold mb-4">Hello, I'm Nathalie</h1>
 
           <p className="lg:w-[80%] 4xl:w-[60%] font-light md:text-lg 2xl:text-xl text-center md:leading-8 2xl:leading-9 dark:text-neutral-300">
-            This space is a caboodle of my daily ramblings – an abode for my undistributed thoughts, serene thinking and tranquil ideas. Today, I welcome you to my blog, and these are my misadventures!
+            This space is a caboodle of my daily ramblings – an abode for my undistributed thoughts, pseudo pensieve and infinite loops. Today, I welcome you to my blog, and these are my misadventures!
           </p>
         </div>
       </BlurFade>
